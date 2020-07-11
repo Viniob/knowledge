5a752ac256e3bcd650bd828a3e44cd18d970e993
Vinicius Baptista <vinao_ob@hotmail.com>
2020-07-11 12:46:44 -0300

    function delete book byId

--- knowledge/src/main/java/com/br/knowledge/controller/BookController.java
+++ knowledge/src/main/java/com/br/knowledge/controller/BookController.java
@@ -1,15 +1,20 @@
 package com.br.knowledge.controller;
 
+import java.net.http.HttpResponse;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.br.knowledge.model.Book;
@@ -38,4 +43,16 @@ public class BookController {
 		return new ResponseEntity<List<Book>>(bookRepository.findAll(), HttpStatus.OK);
 	}
 
+	@PreAuthorize("hasRole('admin') or hasRole('user')")
+	@CrossOrigin(allowedHeaders = "*")
+	@DeleteMapping("/deleteBook")
+	public ResponseEntity<?> deleteBook(@RequestParam String id) {
+		Optional<Book> existBookRepo = bookRepository.findById(id);
+		if (existBookRepo.isPresent()) {
+			bookRepository.deleteById(id);
+			return new ResponseEntity<>(HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+	}
 }
